b81b74b4fbce8e64502832c1fcbc1b2e55deafe7
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-04-27 17:22:16 +0700

    first commit

---
+++ rice-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Component/Navbar/Navbar";
+import "./index.css";
+
+function App() {
+  return (
+    <div className='container'>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
