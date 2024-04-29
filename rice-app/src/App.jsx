d0810eed3ab4102eee7d0901836a9a7687bdf6e9
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-04-29 16:57:34 +0700

    added nav routes

--- rice-app/src/App.jsx
+++ rice-app/src/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import Navbar from "./Component/Navbar/Navbar";
+import Home from "./Component/Home";
+import Shop from "./Component/Shop";
+import Service from "./Component/Services";
+import Contact from "./Component/Contact";
+import About from "./Component/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 function App() {
   return (
     <div className='container'>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/service' element={<Service />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
