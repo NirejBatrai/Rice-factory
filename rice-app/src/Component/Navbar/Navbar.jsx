fce569f3d49bfe6a10d5298763ea05e098ff2f6f
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-04-29 16:58:34 +0700

    added navlinks

--- rice-app/src/Component/Navbar/Navbar.jsx
+++ rice-app/src/Component/Navbar/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from "react";
-import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div className='navbar'></div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/service'>Service</Link>
+        </li>
+        <li>
+          <Link to='/shop'>Shop</Link>
+        </li>
+        <li>
+          <Link to='/contact'>Contact</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
