ab3461b05ea73b949b2d7c159d608cb191d3410b
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-05-02 17:56:04 +0700

    added class container

--- rice-app/src/Component/Navbar/Navbar.jsx
+++ rice-app/src/Component/Navbar/Navbar.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/service'>Service</Link>
-        </li>
-        <li>
-          <Link to='/shop'>Shop</Link>
-        </li>
-        <li>
-          <Link to='/contact'>Contact</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-      </ul>
+    <nav className='navbar'>
+      <div className='container'>
+        <h3 className='logo'> Logo</h3>
+
+        <ul className='nav-links'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/service'>Service</Link>
+          </li>
+          <li>
+            <Link to='/shop'>Shop</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
